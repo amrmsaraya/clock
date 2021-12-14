f460d751706939181a6edf0fea0516daeb4690b7
Amr Saraya <amrmsaraya@gmail.com>
2021-12-14 05:36:55 +0200

    NavigationBarIcons: Use outlined icons always (active & inactive)

--- app/src/main/java/com/github/amrmsaraya/clock/presentation/navigation/Screens.kt
+++ app/src/main/java/com/github/amrmsaraya/clock/presentation/navigation/Screens.kt
@@ -2,10 +2,6 @@ package com.github.amrmsaraya.clock.presentation.navigation
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Alarm
-import androidx.compose.material.icons.filled.HourglassFull
-import androidx.compose.material.icons.filled.Timer
-import androidx.compose.material.icons.filled.WatchLater
 import androidx.compose.material.icons.outlined.Alarm
 import androidx.compose.material.icons.outlined.HourglassEmpty
 import androidx.compose.material.icons.outlined.Timer
@@ -16,34 +12,34 @@ import com.github.amrmsaraya.clock.R
 sealed class Screens(
     val route: String,
     @StringRes val title: Int,
-    val activeIcon: ImageVector = Icons.Filled.WatchLater,
+    val activeIcon: ImageVector = Icons.Outlined.WatchLater,
     val inactiveIcon: ImageVector = Icons.Outlined.WatchLater
 ) {
     object Clock : Screens(
         route = "clock",
         title = R.string.clock,
-        activeIcon = Icons.Filled.WatchLater,
+        activeIcon = Icons.Outlined.WatchLater,
         inactiveIcon = Icons.Outlined.WatchLater
     )
 
     object Alarm : Screens(
         route = "alarm",
         title = R.string.alarm,
-        activeIcon = Icons.Filled.Alarm,
+        activeIcon = Icons.Outlined.Alarm,
         inactiveIcon = Icons.Outlined.Alarm
     )
 
     object Stopwatch : Screens(
         route = "stopwatch",
         title = R.string.stopwatch,
-        activeIcon = Icons.Filled.Timer,
+        activeIcon = Icons.Outlined.Timer,
         inactiveIcon = Icons.Outlined.Timer
     )
 
     object Timer : Screens(
         route = "timer",
         title = R.string.timer,
-        activeIcon = Icons.Filled.HourglassFull,
+        activeIcon = Icons.Outlined.HourglassEmpty,
         inactiveIcon = Icons.Outlined.HourglassEmpty
     )
 }
