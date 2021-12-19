d630c1663033d7f2d0edd30e14b10dd21b944950
Amr Saraya <amrmsaraya@gmail.com>
2021-12-19 13:49:50 +0200

    AlarmDialog: set default time to the current time

--- domain/src/main/java/com/github/amrmsaraya/clock/domain/entity/Alarm.kt
+++ domain/src/main/java/com/github/amrmsaraya/clock/domain/entity/Alarm.kt
@@ -1,11 +1,13 @@
 package com.github.amrmsaraya.clock.domain.entity
 
+import java.util.*
+
 data class Alarm(
     val id: Long = 0,
     val title: String = "",
-    val hour: Int = 9,
-    val minute: Int = 0,
-    val amPm: Int = 0,
+    val hour: Int = Calendar.getInstance().get(Calendar.HOUR),
+    val minute: Int = Calendar.getInstance().get(Calendar.MINUTE),
+    val amPm: Int = Calendar.getInstance().get(Calendar.AM_PM),
     val color: Int = 0,
     val repeatOn: List<Int> = emptyList(),
     val ringtone: String = "",
