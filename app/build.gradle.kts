594cc68fcf1cd6defef5763e46e9f26aa4bbe7c4
Amr Saraya <amrmsaraya@gmail.com>
2022-01-10 01:37:04 +0200

    Pump version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.github.amrmsaraya.clock"
         minSdk = Versions.minSdk
         targetSdk = Versions.targetSdk
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
