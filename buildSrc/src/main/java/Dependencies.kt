9b42dbe5e7d7573d4bbb5f0cc2f43a6214f0c28a
Amr Saraya <amrmsaraya@gmail.com>
2022-01-29 02:18:55 +0200

    Pump version to 1.0.5

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -14,8 +14,8 @@ object AndroidConfig {
     const val jvmTarget = "1.8"
 
     // Version
-    const val versionCode = 5
-    const val versionName = "1.0.4"
+    const val versionCode = 6
+    const val versionName = "1.0.5"
 }
 
 object Versions {
